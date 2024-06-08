86f8dc3f625f5426694442ed7ac25a811c986e92
shigueru nagata <shiguerunagata@protonmail.com>
2024-06-08 02:28:13 -0400

    se creo esqueleto del programa para mantener el orden.
    Se tienen secciones para:
      - constantes
      - variables
      - main
      - funciones
      - subrutinas

---
+++ fortran/load.f90
@@ -0,0 +1,24 @@
+! ----
+! Nombre: load.f90
+! Autor: Shigueru Nagata
+! Descripcion: Programa para leer informacion
+!              de archivos de gaussian 16
+! ----
+
+program load
+implicit none
+
+! <<< constantes >>>
+
+character(len=11), parameter :: InputFile="Agua_MO.log" ! archivo de entrada
+
+! <<< variables >>>
+
+! <<< main >>>
+
+! <<< funciones >>>
+
+! <<< sub-rutinas >>>
+
+end program load
+
